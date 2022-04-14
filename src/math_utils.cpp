93c1eb44a184f3a39d92fa7b9c875cb1cf0fa3d2
colt <colten@utexas.edu>
2022-04-13 20:51:01 -0500

    Add 2d point struct Add interpolation function

--- src/math_utils.cpp
+++ src/math_utils.cpp
@@ -1,5 +1,19 @@
+#include "math_utils.h"
+
 #include <cmath>
 
-float deg_to_rad(float deg) {
+Point Point::operator*(const float scalar) {
+  return { this->x * scalar, this->y * scalar };
+}
+
+float deg_to_rad(const float deg) {
   return deg * (float)M_PI / 180.f;
+}
+
+float dist(Point p0, const Point p1) {
+  return sqrt(pow(p1.x - p0.x, 2) + pow(p1.y - p0.y, 2));
+}
+
+Point interpolate(float t, Point p0, Point p1, Point p2, Point p3) {
+  return p0 * (pow(-t, 3) + 3 * pow(t, 2) - 3 * t + 1);
 }
--- src/math_utils.h
+++ src/math_utils.h
@@ -3,7 +3,17 @@
 
 #include <algorithm>
 
+struct Point {
+  Point operator*(const float scalar);
+  float x;
+  float y;
+};
+
+#include "imgui.h"
+
 float deg_to_rad(float deg);
+
+Point interpolate(float t, Point p0, Point p1, Point p2, Point p3);
 
 template <typename T>
 T clamp(const T& x, const T& min_val, const T& max_val) {
